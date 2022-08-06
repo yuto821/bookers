5e831db281cba412364d445d5fae125a1b4d6719
EC2 Default User <ec2-user@ip-172-31-38-126.ap-northeast-1.compute.internal>
2022-08-06 04:27:06 +0000

    [Finish]

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -4,25 +4,47 @@ class BooksController < ApplicationController
   end
 
   def index
+    @books=Book.all
     @book=Book.new
   end
 
   def create
-    book=Book.new(book_params)
-    book.save
-    redirect_to '/books/show'
+    @book=Book.new(book_params)
+  if @book.save
+    redirect_to book_path(@book), notice: 'Book was successfully created.'
+  else
+    @books=Book.all
+    render :index
+  end
   end
 
   def show
+    @book=Book.find(params[:id])
   end
 
   def edit
     @book=Book.find(params[:id])
   end
 
-    private
-  # ストロングパラメータ
+  def update
+    @book = Book.find(params[:id])
+  if @book.update(book_params)
+    redirect_to book_path(@book.id), notice: 'Book was successfully Updated.'
+  else
+    @books=Book.all
+    render :edit
+  end
+  end
+
+  def destroy
+    book = Book.find(params[:id])
+    book.destroy
+    redirect_to '/books'
+  end
+
+  private
+
   def book_params
-    params.require(:book).permit(:title, :body)
+    params.require(:book).permit(:title,:body)
   end
 end
